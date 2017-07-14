444d4e9920f98b9cd995abc4c5276334bdba0fb1
Steev Pierce <steev.pierce@enginegroup.com>
2017-07-14 15:22:28 +0100

    Working on Cursor.

--- app/src/main/java/com/weirdresonance/android/rocksguitarinventory/InventoryActivity.java
+++ app/src/main/java/com/weirdresonance/android/rocksguitarinventory/InventoryActivity.java
@@ -1,6 +1,12 @@
 package com.weirdresonance.android.rocksguitarinventory;
 
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -8,8 +14,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class InventoryActivity extends AppCompatActivity {
+import com.weirdresonance.android.rocksguitarinventory.data.InventoryContract;
+import com.weirdresonance.android.rocksguitarinventory.data.InventoryContract.InventoryEntry;
+
+
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    // TODO: Comments needed review below
+    /** Database helper that will provide us access to the database */
+    //private PetDbHelper mDbHelper;
+    private static final int PRODUCT_LOADER = 0;
+
+    InventoryCursorAdapter mCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +47,48 @@ public class InventoryActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // TODO: Update comments here for the items below.
+        // To access our database, we instantiate our subclass of SQLiteOpenHelper
+        // and pass the context, which is the current activity.
+        //mDbHelper = new PetDbHelper(this);
+
+
+        // Find the ListView which will be populated with the pet data
+        ListView productListView = (ListView) findViewById(R.id.product_list);
+
+        // Find and set empty view on the ListView, so that it only shows when the list has 0 items.
+        View emptyView = findViewById(R.id.empty_view);
+        productListView.setEmptyView(emptyView);
+
+        // Setup an adapter to create a list item for each row of pet data in the DB.
+        mCursorAdapter = new InventoryCursorAdapter(this, null);
+        productListView.setAdapter(mCursorAdapter);
+
+        // Define the click listener.
+        productListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // Create new intent to go to {@link EditorActivity}
+                Intent intent = new Intent(InventoryActivity.this, EditorActivity.class);
+
+                // Form the content URI that represents the specific pet that was clicked on,
+                // by appending the "id" (passed as input to this method) onto the
+                // {@link PetEntry#CONTENT_URI}.
+                // For example, the URI would be "content://com.example.android.pets/pets/2"
+                // if the pet with ID 2 was clicked on.
+                Uri currentProductUri = ContentUris.withAppendedId(InventoryEntry.CONTENT_URI, id);
+
+                // Set the URI on the data field of the intent
+                intent.setData(currentProductUri);
+
+                // Launch the {@link EditorActivity} to display the data for the current pet.
+                startActivity(intent);
+            }
+        });
+
+        getLoaderManager().initLoader(PRODUCT_LOADER, null, this);
     }
 
     @Override
@@ -50,5 +111,34 @@ public class InventoryActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        // Define a projection that specifies the columns from the table we care about.
+        String[] projection = {
+                InventoryEntry._ID,
+                InventoryEntry.COLUMN_PRODUCT_NAME,
+                InventoryEntry.COLUMN_PRODUCT_PRICE };
+
+        // This loader will execute the ContentProvider's query method on a background thread
+        return new CursorLoader(this,   // Parent activity context
+                InventoryEntry.CONTENT_URI,   // Provider content URI to query
+                projection,             // Columns to include in the resulting Cursor
+                null,                   // No selection clause
+                null,                   // No selection arguments
+                null);                  // Default sort order
+
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
---
+++ app/src/main/java/com/weirdresonance/android/rocksguitarinventory/InventoryCursorAdapter.java
@@ -0,0 +1,50 @@
+package com.weirdresonance.android.rocksguitarinventory;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.weirdresonance.android.rocksguitarinventory.data.InventoryContract;
+
+import org.w3c.dom.Text;
+
+import static android.R.attr.name;
+
+/**
+ * Created by Stephen.Pierce on 14/07/2017.
+ */
+
+public class InventoryCursorAdapter extends CursorAdapter {
+
+    public InventoryCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0 /* flags */);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        TextView nameTextView = (TextView) view.findViewById(R.id.product_name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.product_price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.product_quantity);
+
+        int nameColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryContract.InventoryEntry.COLUMN_PRODUCT_QUANTITY);
+
+        String productName = cursor.getString(nameColumnIndex);
+        String productPrice = cursor.getString(priceColumnIndex);
+        String productQuantity = cursor.getString(quantityColumnIndex);
+
+        nameTextView.setText(productName);
+        priceTextView.setText(productPrice);
+        quantityTextView.setText(productQuantity);
+    }
+}
